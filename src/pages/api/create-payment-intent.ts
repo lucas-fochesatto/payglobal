52d13a90a6f491be1ef4f7b2cda9fe9ee4aaeb7b
lucas-fochesatto <lucas.fochesatto@gmail.com>
2024-06-08 17:55:53 -0300

    usd

--- src/pages/api/create-payment-intent.ts
+++ src/pages/api/create-payment-intent.ts
@@ -16,7 +16,7 @@ export default async function handler(req : NextApiRequest, res : NextApiRespons
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "brl",
+    currency: "usd",
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
       enabled: true,
